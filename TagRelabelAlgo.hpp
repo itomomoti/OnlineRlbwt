ce7da30e5775c75c86d066f090a90dd6dbc428c9
Tomohiro I <itomomoti@gmail.com>
2017-10-12 12:22:00 +0900

    tiny change

--- TagRelabelAlgo.hpp
+++ TagRelabelAlgo.hpp
@@ -20,11 +20,11 @@
 
 /*!
  * @brief Utilities to implement linked-lists that can answer order queries in O(1) time using Tag-range Relabeling Algorithm (TRA).
- * @par Reference
+ * @par References
  *   [1] Bender, M. A.; Cole, R.; Demaine, E. D.; Farach-Colton, M. & Zito, J. Two Simplified Algorithms for Maintaining Order in a List, ESA, 2002, 152-164.
  * @par Notation
  *   A traCode is uint ranging from 9 to 15, which determines the threshold of overflow (it corresponds to parameter T of [1] but not exactly the same).
- *   More precisely, a tag-range sharing '64 - x' most significant bits is said to be overflow iff the # of elements in the tag-range is larger than '(traCode/8)^x'.
+ *   More precisely, a tag-range sharing "64 - x" most significant bits is said to be overflow iff the #elements in the tag-range is larger than "(traCode/8)^x".
  */
 struct TagRelabelAlgo
 {
@@ -33,15 +33,15 @@ struct TagRelabelAlgo
   static constexpr uint8_t NUM_TRACODE{7};
   static constexpr uint64_t MAX_LABEL{ctcbits::UINTW_MAX(63)};
   /*!
-   * @brief TBL_Capacities[i] represents the max # of the elements handled with traCode of 'i + 9'.
+   * @brief TBL_Capacities[i] represents the max #elements handled with traCode of "i + 9".
    * @note
-   *   TBL_Capacities[i] is roughly '((i+9)/8)^63' (not exactly so due to rounding up for each multiplication of '((i+9)/8)').
+   *   TBL_Capacities[i] is roughly "((i+9)/8)^63" (not exactly so due to rounding up for each multiplication of "((i+9)/8)").
    */
   static const uint64_t TBL_Capacities[NUM_TRACODE];
 
 
   /*!
-   * @brief Get the smallest traCode to handle 'num' elements.
+   * @brief Get the smallest traCode to handle "num" elements.
    */
   static uint8_t getSmallestTraCode(uint64_t num) noexcept {
     assert(TBL_Capacities[NUM_TRACODE-1] >= num);
@@ -55,7 +55,7 @@ struct TagRelabelAlgo
 
   
   /*!
-   * @brief Multiply '(traCode/8)' to 'overflowNum'.
+   * @brief Multiply "(traCode/8)" to "overflowNum".
    */
   static uint64_t getNextOverflowNum(uint64_t overflowNum, uint8_t traCode) noexcept {
     return ((overflowNum * traCode) >> 3) + 1;
